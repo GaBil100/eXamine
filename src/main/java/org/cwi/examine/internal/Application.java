11a88adf2ea21c5fe99b8ecffacb75d502ee61b9
Gareth Bilaney <gareth.bilaney@hhu.de>
2017-12-20 17:45:26 +0100

    Update Application.java

--- src/main/java/org/cwi/examine/internal/Application.java
+++ src/main/java/org/cwi/examine/internal/Application.java
@@ -3,7 +3,7 @@ package org.cwi.examine.internal;
 import org.cwi.examine.internal.data.DataSet;
 import org.cwi.examine.internal.visualization.Visualization;
 import org.cwi.examine.internal.model.Model;
-
+import org.cwi.examine.internal.molepan.dataread.DataRead;
 import java.io.IOException;
 
 /**
@@ -12,6 +12,8 @@ import java.io.IOException;
 public class Application {
 
     public static void main(String[] args) throws IOException {
+    
+    	DataRead dataread = new DataRead();
         final DataSet dataSet = new DataSet();
         final Model model = new Model(dataSet);
         final Visualization visualization = new Visualization(model);

a6d4a171faba0030f98e4cf87b11d2e1b60e5b50
Gareth Bilaney <gareth.bilaney@hhu.de>
2018-02-28 18:08:58 +0100

    Create Option.java

---
+++ src/main/java/org/cwi/examine/internal/Option.java
@@ -0,0 +1,37 @@
+package org.cwi.examine.internal;
+
+import org.cwi.examine.internal.data.DataSet;
+import org.cwi.examine.internal.visualization.Visualization;
+import org.cwi.examine.internal.model.Model;
+import org.cwi.examine.internal.molepan.dataread.DataRead;
+
+import org.cwi.examine.internal.ui.controller.Main;
+
+
+import java.io.IOException;
+
+
+public class Option {
+	private static boolean scel;
+
+	public static boolean getScel(){
+		return scel;
+	}
+	
+	public void setScel (String a){
+
+	if(a.contains("On")){
+
+		scel = true;
+	}
+		
+	else if(scel != true){
+		scel = false;
+	}
+	
+	}
+	
+	
+
+
+}

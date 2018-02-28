df0308e279567755804cd602c99db65b0f4efef5
Gareth Bilaney <gareth.bilaney@hhu.de>
2018-02-28 18:09:16 +0100

    Update Application.java

--- src/main/java/org/cwi/examine/internal/Application.java
+++ src/main/java/org/cwi/examine/internal/Application.java
@@ -4,6 +4,10 @@ import org.cwi.examine.internal.data.DataSet;
 import org.cwi.examine.internal.visualization.Visualization;
 import org.cwi.examine.internal.model.Model;
 import org.cwi.examine.internal.molepan.dataread.DataRead;
+
+import org.cwi.examine.internal.ui.controller.Main;
+
+import org.cwi.examine.internal.Option;
 import java.io.IOException;
 
 /**
@@ -13,6 +17,14 @@ public class Application {
 
     public static void main(String[] args) throws IOException {
     
+    
+    	Option option = new Option();
+    	option.setScel(args[0]);
+    	System.out.println( option.getScel() );
+    	
+    	
+    	
+    	
     	DataRead dataread = new DataRead();
         final DataSet dataSet = new DataSet();
         final Model model = new Model(dataSet);

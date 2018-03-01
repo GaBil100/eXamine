f1a871a62e00aff7404bc43d28c0b66866f48fea
Gareth Bilaney <gareth.bilaney@hhu.de>
2018-03-01 18:52:56 +0100

    File Import (work in progress)

--- src/main/java/org/cwi/examine/internal/ui/controller/Main.java
+++ src/main/java/org/cwi/examine/internal/ui/controller/Main.java
@@ -1,19 +1,65 @@
-package org.cwi.examine.internal.ui.controller;
-
-
+package controller;
+	
+import java.io.File;
 import java.io.IOException;
 import javafx.fxml.FXMLLoader;
-
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.application.Application;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-
+import javafx.scene.layout.VBox;
 import javafx.scene.layout.AnchorPane;
 
 
 public class Main extends Application {
 	private Stage primaryStage;
+	
+	
+	
+	
+	
+	
+	
+	
+	 @Override
+	    public void start(final Stage stage) throws Exception {
+	        Button button = new Button("Choose");
+	        Label chosen = new Label();
+	        button.setOnAction(event -> {
+	            FileChooser chooser = new FileChooser();
+	            File file = chooser.showOpenDialog(stage);
+	            if (file != null) {
+	                String fileAsString = file.toString();
+
+	                chosen.setText("Chosen: " + fileAsString);
+	            } else {
+	                chosen.setText(null);
+	            }
+	        });
+
+	        VBox layout = new VBox(10, button, chosen);
+	        layout.setMinWidth(400);
+	        layout.setAlignment(Pos.CENTER);
+	        layout.setPadding(new Insets(10));
+	        stage.setScene(new Scene(layout));
+	        stage.show();
+	    }
+
+	
+	
+	
+	
+	
+	
+	
+	/*
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
@@ -50,8 +96,8 @@ public class Main extends Application {
 		{
 			e.printStackTrace();
 		}
-	}
-	
+	}  */
+	 
 	public static void main(String[] args) {
 		launch(args);
 	}
